9a2d2f2d3b3cabbce9fc7fec0f1d5e09de279618
Prachee Abitkar <prachee.abitkar@dgsl.in>
2023-10-26 16:13:43 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 /*global chrome*/
 import './App.css';
-
-import { BrowserRouter as Router, Routes, Route, Link, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ForgetPassword from './forgetPassword/forgetPassword';
-import StartStop from './StartStop/StartStop';
-import { Button, Image, Modal, Form, Row, Col } from 'react-bootstrap'
-import { Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import backgroundimg from './assets/Images/background.svg';
-import start from './assets/Images/start.svg';
-import stop from './assets/Images/stop.svg';
 import React, { useState, useEffect } from 'react';
-import RecordRTC from 'recordrtc';
 import Login from './login/login';
 import AudioRecorder from "./AudioRecorder";
 import Sidebar from './sidebar/Sidebar';
@@ -19,32 +11,18 @@ import Forgetpasschange from './forgetPassword/forgetpasschange';
 import ListOfRequirements from './ListOfRequirements/ListOfRequirements';
 import ListOfCandidate from './ListOfCandidate/ListOfCandidate';
 import CandidateDetails from './CandidateDetails/CandidateDetails';
-import Recording from './Recording/Recording';
 function App() {
-
-  // In your React component after successful login
-  var isLoggedIn = true;
-  console.log(isLoggedIn, "isLoggedIn")
 
 
   useEffect(() => {
 
-
     if (typeof chrome !== 'undefined' && chrome.runtime && chrome.runtime.sendMessage) {
-      // In your React component after successful login
-      // const loginStatus = true; // Set to true after successful login
-      // chrome.runtime.sendMessage({ loginStatus }, response => {
-      //   if (response && response.message === 'LoginStatusReceived') {
-      //     console.log('Login status sent to the extension.');
-      //   }
-      // });
+   
       const data = localStorage.getItem('token')
       chrome.runtime.sendMessage({
         action: 'my-action',
         data,
       });
-
-      
 
     } else {
       // You're not in a Chrome extension context
@@ -66,7 +44,6 @@ function App() {
           <Route exact path='/listofrequirements' element={<ListOfRequirements />} />
           <Route exact path='/listofcandidate' element={<ListOfCandidate />} />
           <Route exact path='/candidatedetails/:id' element={<CandidateDetails />} />
-          {/* <Route exact path='/recording' element={<Recording />} /> */}
           <Route exact path='/recording' element={<AudioRecorder />} />
 
           <Route exact path='/resetPassword' element={< Forgetpasschange />} />
--- src/ListOfCandidate/ListOfCandidate.js
+++ src/ListOfCandidate/ListOfCandidate.js
@@ -1,6 +1,6 @@
 
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Card, Container, Row, Col, Form, FormGroup, Label, Input, Button, Image, Table } from 'react-bootstrap';
 import Sidebar from './../sidebar/Sidebar';
 import './ListOfCandidate.css';
@@ -8,117 +8,36 @@ import logo from '../assets/Images/logo.svg';
 import profile from '../assets/Images/profile.svg';
 import back from '../assets/Images/back.svg';
 import { useNavigate, useLocation } from 'react-router-dom';
-const rowsPerLoad = 5;
-
-const CustomInput = ({ value, onChange, onClear }) => {
-  const handleChange = (e) => {
-    onChange(e.target.value);
-  };
-
-  return (
-    <>
-      <div className="input-container">
-        <div className="input-wrapper">
-          <Form.Control
-            type="text"
-            value={value}
-            onChange={handleChange}
-            className="custom-input listinput"
-          />
-          {value && (
-            <span className="clear-icon" onClick={onClear}>
-              &times;
-            </span>
-          )}
-        </div>
-      </div>
-
-    </>
-  );
-}
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import Loader from '../Loader';
+import { useDispatch, useSelector } from "react-redux"
+import { logout } from '../redux/authSlice';
+import logout1 from '../assets/Images/logout.svg'
+import home from '../assets/Images/home.svg'
+import search from '../assets/Images/search1.svg'
+import book from '../assets/Images/book.svg'
 const ListOfCandidate = () => {
-  const [inputText, setInputText] = useState('');
   let navigate = useNavigate();
-  // const [data, setData] = useState([]);
   const [displayedData, setDisplayedData] = useState([]);
-  const [start, setStart] = useState(0);
+  const [candidate, setCandidate] = useState([]);
+  const [next1, setnext1] = useState('');
+  const [previous, setprevious] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [userdata,setuserdata]=useState([])
 
   const location = useLocation();
   const data = location.state?.candidatedata || [];
-  const userjson = location.state?.userjson || [];
-
-
-  console.log(userjson, 'candidatedata')
-  // useEffect(() => {
-  //   // Simulate fetching data from an API
-  //   // Replace this with your data-fetching logic
-  //   const fetchData = async () => {
-  //     // Simulating API call delay
-  //     await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  //     // Assuming you have an array of data
-  //     // const newData = Array.from({ length: data.length + rowsPerLoad }, (_, index) => ({
-  //     //   id: index + 1,
-  //     //   candidate: 'Joey M.',
-  //     //   match: '9/10 ',
-  //     //   skills: 'React.js',
-  //     //   contact: 'joeym@gamil.com'
-  //     // }));
-
-
-  //     const newData = [
-  //       {
-  //         id: "AWB123456",
-  //         candidate: 'Joey M.',
-  //         match: '9/10',
-  //         skills: 'React.js',
-  //         contact: 'joeym@gamil.com'
-  //       },
-  //       {
-  //         id: "AWB123456",
-  //         candidate: 'Joey M.',
-  //         match: '6/10',
-  //         skills: 'React.js',
-  //         contact: 'joeym@gamil.com'
-  //       },
-  //       {
-  //         id: "AWB123456",
-  //         candidate: 'Joey M.',
-  //         match: '4/10',
-  //         skills: 'React.js',
-  //         contact: 'joeym@gamil.com'
-  //       },
-
-
-  //     ];
-
-  //     setData(newData);
-  //   };
-
-  //   fetchData();
-  // }, [data]);
-
-  const getColorClass = (value) => {
-
-
-    if (value > '6/10') {
-      return 'green-text';
-    } else if (value < '5/10') {
-      return 'red-text';
-    } else if (value >= '5/10' && value <= '6/10') {
-      return 'yellow-text';
-    } else {
-      return ''; // No special class for values between 5 and 8
-    }
-  };
+  const trsncriptiondata = location.state?.trsncriptiondata || [];
+  const userdata1 = location.state?.userdata || [];
+  
   useEffect(() => {
-    // Update the displayedData based on start and rowsPerLoad
-    setDisplayedData(data.slice(0, start + rowsPerLoad));
-  }, [data, start]);
-
-  const loadMoreRows = () => {
-    setStart(start + rowsPerLoad);
-  };
+    setuserdata(userdata1)
+    setCandidate(data);
+    setDisplayedData(data.results);
+    setnext1(data.next_page)
+    setprevious(data.previous_page)
+  }, []);
 
   const [jobTitle, setjobTitle] = useState('');
   const [skill, setskill] = useState('');
@@ -127,11 +46,11 @@ const ListOfCandidate = () => {
 
   useEffect(() => {
 
-    if (userjson) {
-      setjobTitle(userjson?.openai_response?.experience_title)
-      setskill(userjson?.openai_response?.skills_Required)
-      setlocation(userjson?.openai_response?.job_location)
-      setIndustry(userjson?.openai_response?.industry)
+    if (trsncriptiondata) {
+      setjobTitle(trsncriptiondata?.data?.experience_title)
+      setskill(trsncriptiondata?.data?.keyword)
+      setlocation(trsncriptiondata?.data?.location)
+      setIndustry(trsncriptiondata?.data?.experience_company_industry)
     }
   }, []);
 
@@ -151,121 +70,249 @@ const ListOfCandidate = () => {
   };
 
   let list = {
-    'experience_title': '',
-    'industry': '',
-    'job_location': '',
-    'skills_Required': ''
+    "data": {
+      "experience_title": "",
+      "keyword": "",
+      "location": "",
+      "experience_company_industry": ""
+    },
   }
   const onSearchClick = (event) => {
     event.preventDefault();
 
+    list.data.experience_title = jobTitle;
+    list.data.keyword = skill ;
+    list.data.location = location1;
+    list.data.experience_company_industry = industry;
 
-    list.experience_title = jobTitle;
-    list.industry = industry;
-    list.job_location = location1;
-    list.skills_Required = skill;
-  console.log(list,"list")
-    // axios
-    //   .post('https://flat-star-41744.botics.co/profile/final/', list, {
-    //     headers: {
-    //       'Content-Type': 'multipart/form-data',
-    //       'Authorization': `token ${localStorage.getItem('token')}`
-    //     },
-    //   })
-    //   .then((response) => {
-    //     console.log('Audio sent to backend:', response);
-   
+    setLoading(true)
+    axios
+      .post('https://flat-star-41744.botics.co/profile/final/?page=1&page_size=5', JSON.stringify(list.data), {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `token ${localStorage.getItem('token')}`
+        },
+      })
+      .then((response) => {
+        setLoading(false)
+        setCandidate(response.data);
+        console.log('Audio sent to backend final:', response.data.results);
+        setDisplayedData(response.data.results)
+        setnext1(response.data.next_page)
+        setprevious(response.data.previous_page)
 
-    //   })
-    //   .catch((error) => {
-    //     console.error('Error sending audio to backend:', error);
-  
-    //   });
+      })
+      .catch((error) => {
+        console.error('Error sending audio to backend:', error);
+
+      });
   }
 
+  function onCandidateClick(editid) {
+
+    list.data.experience_title = jobTitle;
+    list.data.keyword = skill ;
+    list.data.location = location1;
+    list.data.experience_company_industry = industry;
+
+
+    const dispdata = displayedData.filter((d) => d.candidate_id == editid);
+    navigate(`/candidatedetails/${editid}`, {
+      state: {
+        candidatedata: dispdata,
+        candidate:candidate,
+        trsncriptiondata:list,
+        userdata:userdata  
+        
+      }
+    })
+  }
+  function nextbuttonCLick() {
+    setLoading(true)
+
+    list.data.experience_title = jobTitle;
+    list.data.keyword = skill ;
+    list.data.location = location1;
+    list.data.experience_company_industry = industry;
+
+    axios
+    .post(next1, JSON.stringify(list.data), {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `token ${localStorage.getItem('token')}`
+      },
+    })
+    .then((response) => {
+      setLoading(false)
+      console.log('Audio sent to backend final:', response.data.results);
+      setDisplayedData(response.data.results)
+      setnext1(response.data.next_page)
+      setprevious(response.data.previous_page)
+
+
+    })
+    .catch((error) => {
+      console.error('Error sending audio to backend:', error);
+
+    });
+
+  }
+
+  function previousbuttonCLick() {
+    setLoading(true)
+    list.data.experience_title = jobTitle;
+    list.data.keyword = skill ;
+    list.data.location = location1;
+    list.data.experience_company_industry = industry;
+
+    axios
+      .post(previous, JSON.stringify(list.data), {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `token ${localStorage.getItem('token')}`
+        },
+      })
+      .then((response) => {
+        setLoading(false)
+        console.log('Audio sent to backend final:', response.data.results);
+        setDisplayedData(response.data.results)
+        setnext1(response.data.next_page)
+        setprevious(response.data.previous_page)
+
+
+      })
+      .catch((error) => {
+        console.error('Error sending audio to backend:', error);
+
+      });
+  }
+  const [activeMenuItem, setActiveMenuItem] = useState();
+  const dispatch = useDispatch()
+  // Function to handle menu item clicks
+  const handleMenuItemClick = (menuItem) => {
+    setActiveMenuItem(menuItem);
+    const menunavigate='/'+ menuItem;
+    navigate(menunavigate)
+  };
+
+  const onLogout = () => {
+    navigate('/')
+    dispatch(logout())
+   
+  }
+
+
+  function transcribeData() {
+ 
+    navigate(`/listofrequirements`, {
+      state: {
+        userdata: userdata,
+      }
+    })
+  }
+  
   return (
     <>
       <div className='headerlogo'>
         <Image src={logo} alt="Image" className='mainlogo' />
-        {/* <div className='profileheader'>
-          <div class="profile-image">
-            <Image src={profile} alt="Image" className='profileimg' />
-
-          </div>
-          <div class="profile-name">
-            Monica R.
-          </div>
-        </div> */}
       </div>
-      <Sidebar />
+    
+      <nav className="sidebar">
+        <ul>
+        {/* <li className='sidebarTitle p-4'>Stovsky</li> */}
+          <li
+            className={`${(activeMenuItem == "Home") ? 'active' : ''} py-3 `}
+            onClick={() => handleMenuItemClick('listofrequirements')}
+          >
+             <Image src={home}  className='navbarimg'  />
+            Home
+          </li>
+          <li
+          className={`${(activeMenuItem == "listofcandidate") ? 'active' : ''} py-3 `}
+            onClick={() => handleMenuItemClick('listofcandidate')}
+          >
+            <Image src={search}  className='navbarimg'  />
+            Search Candidates
+          </li>
+          <li
+           className={`${(activeMenuItem == "listofrequirements") ? 'active' : ''} py-3 `}
+            onClick={() => transcribeData()}
+          ><Image src={book}  className='navbarimg'  />
+           Transcribed Data
+          </li>
+          
+        </ul>
+
+        <div onClick={onLogout} className="mt-auto logoutcss1" >
+        <Image src={logout1}  onClick={onLogout}  className='navbarimg'  />
+          Logout</div>
+      </nav>
       <main className="main-content">
         <Container>
-
+        {loading ? (
+          <Loader />
+        ) : (
+          <div>
+          </div>
+        )}
           <Row>
             <Card >
-
               <Card.Body>
-                {/* <div className={`number ${getColorClass()}`}>
-      
-    </div> */}
                 <Card.Title className='cardtitle  m-0 p-0'>Transcription Name </Card.Title>
                 <Form onSubmit={onSearchClick}>
-                <Row className='mt-2'>
-                  <Col className="col-search" lg={3}>
+                  <Row className='mt-2'>
+                    <Col className="col-search" lg={3}>
 
-                    <FormGroup>
-                      <Form.Label className="text-start">Job Title</Form.Label>
-                      <Form.Control
-                        type="text"
-                        defaultValue={jobTitle}
-                        onChange={setjobTitle1}
-                        className="custom-input listinput"
-                      />
-                    </FormGroup>
-                  </Col>
-                  <Col className="col-search" lg={3}>
-                    <FormGroup>
-                      <Form.Label className="text-start">Skills Required</Form.Label>
-                      <Form.Control
-                        type="text"
-                        defaultValue={skill}
-                        onChange={setskill1}
-                        className="custom-input listinput"
-                      />
-                    </FormGroup>
-                  </Col>
+                      <FormGroup>
+                        <Form.Label className="text-start">Job Title</Form.Label>
+                        <Form.Control
+                          type="text"
+                          defaultValue={jobTitle}
+                          onChange={setjobTitle1}
+                          className="custom-input listinput"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col className="col-search" lg={3}>
+                      <FormGroup>
+                        <Form.Label className="text-start">Skills Required</Form.Label>
+                        <Form.Control
+                          type="text"
+                          defaultValue={skill}
+                          onChange={setskill1}
+                          className="custom-input listinput"
+                        />
+                      </FormGroup>
+                    </Col>
 
-                  <Col className="col-search" lg={3}>
-                    <FormGroup>
-                      <Form.Label className="text-start">  Job location</Form.Label>
-                      <Form.Control
-                        type="text"
-                        defaultValue={location1}
-                        onChange={setlocation1}
-                        className="custom-input listinput"
-                      />
-                    </FormGroup>
-                  </Col>
-                  <Col className="col-search" lg={3}>
-                    <FormGroup>
-                      <Form.Label className="text-start"> Industry</Form.Label>
-                      <Form.Control
-                        type="text"
-                        defaultValue={industry}
-                        onChange={setIndustry1}
-                        className="custom-input listinput"
-                      />
-                    </FormGroup>
-                  </Col>
-
-
-
-                </Row>
-                <Row>
-                  <div className='candidatebtn'>
-                    <Button className='listbtncss' type="submit">Search for Candidates</Button>
-                  </div>
-                </Row>
+                    <Col className="col-search" lg={3}>
+                      <FormGroup>
+                        <Form.Label className="text-start">  Job location</Form.Label>
+                        <Form.Control
+                          type="text"
+                          defaultValue={location1}
+                          onChange={setlocation1}
+                          className="custom-input listinput"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col className="col-search" lg={3}>
+                      <FormGroup>
+                        <Form.Label className="text-start"> Industry</Form.Label>
+                        <Form.Control
+                          type="text"
+                          defaultValue={industry}
+                          onChange={setIndustry1}
+                          className="custom-input listinput"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <div className='candidatebtn'>
+                      <Button className='listbtncss' type="submit">Search for Candidates</Button>
+                    </div>
+                  </Row>
                 </Form>
               </Card.Body>
             </Card>
@@ -280,12 +327,11 @@ const ListOfCandidate = () => {
                       <th>Experience Count</th>
                       <th>Skills</th>
                       <th>Location</th>
-                      {/* Add more header columns as needed */}
                     </tr>
                   </thead>
                   <tbody>
-                    {displayedData.map((item) => (
-                      <tr key={item.id} onClick={() => navigate(`/candidatedetails/${item.id}`)} style={{ cursor: 'pointer' }}>
+                    {displayedData?.map((item) => (
+                      <tr key={item.candidate_id} onClick={() => onCandidateClick(item.candidate_id)} style={{ cursor: 'pointer' }}>
                         <td className='candidatenamecss'> <div class="profile-image">
                           <Image src={item.logo_url} alt="Image" className='profileimg' />
 
@@ -293,23 +339,22 @@ const ListOfCandidate = () => {
                             {item.candidate}<br></br>
                             {item.name}
                           </div></td>
-                        <td  style={{width:'125px',textAlign:'center'}}>
-                         {item.experience_count} years
+                        <td style={{ width: '125px', textAlign: 'center' }}>
+                          {item.experience_count} years
                         </td>
                         <td>{item.title}</td>
                         <td>{item.location}</td>
-                        {/* Render additional data columns here */}
                       </tr>
                     ))}
                   </tbody>
 
 
                 </Table>
-                {start + rowsPerLoad < data.length && (
-                  <span className='loadmore' onClick={loadMoreRows}>
-                    Load More
-                  </span>
-                )}
+
+                <div className="pagination">
+                  <Button className='listbtncss mt-2' style={{ marginLeft: '10px' }} type="submit" disabled={next1 != '' && next1 != 'null' && next1 != undefined ? false : true} onClick={() => nextbuttonCLick()}>Next</Button>
+                  <Button className='listbtncss mt-2' style={{ marginLeft: '10px' }} type="submit" disabled={previous != '' && previous != 'null' && previous != undefined ? false : true} onClick={() => previousbuttonCLick()} >Previous</Button>
+                </div>
               </Card.Body>
 
             </Card>
